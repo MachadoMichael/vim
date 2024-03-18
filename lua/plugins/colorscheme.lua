7cb71798f16fdc4101453ab532cca4ef0d1f0401
Michael <machado.m3@icloud.com>
2024-03-18 08:45:47 -0300

    feat: new theme min theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,10 @@
 return {
-  "rose-pine/neovim",
+  "datsfilipe/min-theme.nvim",
   lazy = true,
   priority = 1000,
 }
+
+-- "rose-pine/neovim",
+-- "craftzdog/solarized-osaka.nvim",
+-- "polirritmico/monokai-nightasty.nvim",
+-- "Abstract-IDE/Abstract-cs",
